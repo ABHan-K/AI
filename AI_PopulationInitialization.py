29d4ae4fdaf49767970e7e9a8764b52388062532
ABHan <201830210441@mail.scut.edu.cn>
2020-11-17 09:30:20 +0800

    1.2.1 增加注释

--- AI_PopulationInitialization.py
+++ AI_PopulationInitialization.py
@@ -5,6 +5,9 @@ import random
 
 
 class PopIni:
+    """
+    * 种群初始化类
+    """
     def __init__(self, filename):
         self.net_data = ind.Read(filename)
         self.dynamic_num, self.dynamic_dat = self.net_data.get_data_for_iteration()
@@ -22,5 +25,5 @@ class PopIni:
                 self.dynamic_dat[j, 1], self.dynamic_dat[j, 2] = self.rand_location()  # 生成个体随机坐标
             self.individual[i] = self.individual.DNA(self.dynamic_dat)  # todo 预留创建个体文件
 
-    def pop_data_get(self):  # return 种群数量和种群矩阵
+    def pop_data_get(self):  # return 种群数量和种群矩阵和动态节点个数
         return self.dynamic_num * 100, self.individual, self.dynamic_num
