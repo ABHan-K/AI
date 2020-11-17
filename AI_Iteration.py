606505b5b1bef32a6e69f87b0275ef7aeb36f87a
ABHan <201830210441@mail.scut.edu.cn>
2020-11-17 09:30:40 +0800

    1.2.2 增加注释

--- AI_Iteration.py
+++ AI_Iteration.py
@@ -5,6 +5,10 @@ from random import randint
 
 
 class Iteration:
+    """
+    *运算迭代
+    *计划每一代仅保留三分之一的种群个体数量
+    """
     def __init__(self, filename):
         self.pop_ini = pi.PopIni(filename)
         self.individual_num, self.individual_dat, self.dynamic_num = self.pop_ini.pop_data_get()
@@ -15,6 +19,11 @@ class Iteration:
         self.dead_list = np.zeros(self.individual_num * 2 / 3)
 
     def fit_count(self):
+        """
+
+        :return:当存在无交点图时 个体编号
+                当存在交点图时   -1
+        """
         for i in range(self.individual_num):
             self.fitness[i] = self.fit.fitcount(self.individual_dat[i])
         if 0 in self.fitness:
@@ -23,6 +32,12 @@ class Iteration:
             return -1
 
     def compare(self, a, b, c):
+        """
+        :param a: 个体a
+        :param b: 个体b
+        :param c: 个体c
+        :return: 最适应个体
+        """
         if self.individual_dat[a] < self.individual_dat[b]:
             if self.individual_dat[a] < self.individual_dat[c]:
                 return a
@@ -35,6 +50,11 @@ class Iteration:
                 return c
 
     def killing(self):
+        """
+        每一次选取3个个体进行竞争
+        失败个体放回等待选取
+        直至选择足够数量后停止
+        """
         i = 0
         while i < self.individual_num / 3:
             a = randint(0, self.individual_num - 1)
@@ -51,6 +71,12 @@ class Iteration:
                 i += 1
 
     def reproduction(self, a, b):
+        """
+        万分之一的概率基因突变
+        :param a: 父个体
+        :param b: 母个体
+        :return: 子个体
+        """
         father = self.individual_dat[a]
         mother = self.individual_dat[b]
         son = np.zeros(self.dynamic_num - 1)
@@ -64,6 +90,10 @@ class Iteration:
         return son
 
     def main(self):
+        """
+
+        :return: 迭代结束最优个体
+        """
         a = self.fit_count()
         while a == -1:
             self.killing()
